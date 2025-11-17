fbda18e3a17c4db1170ec4ca6ed579072ca2fdb4
i-defranca <ifranceg@gmail.com>
2025-11-17 13:49:46 -0300

    chore: initial project files - spring initializr

---
+++ src/main/java/dev/challenge/ChallengeApplication.java
@@ -0,0 +1,14 @@
+package dev.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class ChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeApplication.class, args);
+    }
+}
